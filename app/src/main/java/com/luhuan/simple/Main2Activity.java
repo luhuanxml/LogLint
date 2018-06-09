2d13b2b785ca3943c19f98b6a4c4e53e8b26ae81
luhuanxml <luhuanxml@outlook.com>
2018-06-09 14:32:57 +0800

    1111

--- app/src/main/java/com/luhuan/simple/Main2Activity.java
+++ app/src/main/java/com/luhuan/simple/Main2Activity.java
@@ -12,5 +12,6 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Log.d("","");
     }
 }
